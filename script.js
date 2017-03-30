4d1b9b8f311dc2e901d9e76cd2593ee64f1a51a9
Aravind Sripathy <aravind.sripathy@gmail.com>
2017-03-29 18:40:25 -0700

    implemented selection ui

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ var gmApp = angular.module('gmsim', []);
 
 gmApp.controller('gmCtrl', function($scope) {
     $scope.draftSlot = 3;
-    $scope.selections = [4, 2, 3];
+    $scope.options = [0, 1, 2];
+    $scope.selectedIndex = -1;
+    $scope.markSelected = function(number){
+      if($scope.selectedIndex == -1){
+        $scope.selectedIndex = number;
+        console.log(number);
+      }
+    }
 
 });
 
 gmApp.directive("draftPick", function() {
     return {
         template : "<div>Possible Draft Pick</div>"
+
     };
 });
