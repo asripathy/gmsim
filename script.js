6c0c0d43a1ace90d2d865ebf938ae347158dd207
Aravind Sripathy <aravind.sripathy@gmail.com>
2017-03-30 00:09:39 -0700

    small changes

--- script.js
+++ script.js
@@ -2,19 +2,10 @@ var gmApp = angular.module('gmsim', []);
 
 gmApp.controller('gmCtrl', function($scope) {
     $scope.draftSlot = 3;
-    $scope.options = [0, 1, 2];
-    $scope.draftStarted = false;
-    $scope.selectedIndex = -1;
     $scope.validSlot = true;
-    $scope.markSelected = function(number){
-      if($scope.selectedIndex == -1){
-        $scope.selectedIndex = number;
-      }
-    }
-    $scope.generatePicks = function(){
-      $scope.draftStarted = true;
-      console.log("Draft Started!");
-    }
+    $scope.draftStarted = false;
+    $scope.options;
+    $scope.selectedIndex = -1;
     $scope.validateSlot = function(){
       if($scope.slotSelect.slotChoice.$valid){
         $scope.validSlot = true;
@@ -23,7 +14,15 @@ gmApp.controller('gmCtrl', function($scope) {
         $scope.validSlot = false;
       }
     }
-
+    $scope.generatePicks = function(){
+      $scope.draftStarted = true;
+      $scope.options = getPlayersAttributes($scope.draftSlot);
+    }
+    $scope.markSelected = function(number){
+      if($scope.selectedIndex == -1){
+        $scope.selectedIndex = number;
+      }
+    }
 });
 
 getPlayersAttributes();
