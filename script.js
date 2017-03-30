84814e21e48c8b4b7281142d5820957289d2b3ff
Aravind Sripathy <aravind.sripathy@gmail.com>
2017-03-29 23:53:26 -0700

    added pick input and modified ui

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ var gmApp = angular.module('gmsim', []);
 gmApp.controller('gmCtrl', function($scope) {
     $scope.draftSlot = 3;
     $scope.options = [0, 1, 2];
+    $scope.draftStarted = false;
     $scope.selectedIndex = -1;
+    $scope.validSlot = true;
     $scope.markSelected = function(number){
       if($scope.selectedIndex == -1){
         $scope.selectedIndex = number;
-        console.log(number);
+      }
+    }
+    $scope.generatePicks = function(){
+      $scope.draftStarted = true;
+      console.log("Draft Started!");
+    }
+    $scope.validateSlot = function(){
+      if($scope.slotSelect.slotChoice.$valid){
+        $scope.validSlot = true;
+      }
+      else{
+        $scope.validSlot = false;
       }
     }
 
-});
-
-gmApp.directive("draftPick", function() {
-    return {
-        template : "<div>Possible Draft Pick</div>"
-
-    };
 });
